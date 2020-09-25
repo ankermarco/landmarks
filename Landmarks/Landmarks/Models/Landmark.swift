3d2c07965c51d1f61ee2f2e83627c2258eabac37
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-09-25 16:01:18 +0100

    Created Row view

--- Landmarks/Landmarks/Models/Landmark.swift
+++ Landmarks/Landmarks/Models/Landmark.swift
@@ -9,17 +9,41 @@
 import SwiftUI
 import CoreLocation
 
+let landmarkData: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+
 struct Landmark: Hashable, Codable {
     var id: Int
     var name: String
     fileprivate var imageName: String
-    fileprivate var coordinate: Coordinates
+    fileprivate var coordinates: Coordinates
     var state: String
     var park: String
     var category: Category
     
     var locationCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
     }
     
     enum Category: String, CaseIterable, Codable, Hashable {
