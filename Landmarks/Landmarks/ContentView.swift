87bf17c956b37c6d31afd8ebf58481577f453266
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-09-25 13:30:51 +0100

    Added empty Xcode project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ke Ma on 25/09/2020.
+//  Copyright © 2020 Ke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
