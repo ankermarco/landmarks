6d309ee6fdd8d561fd7836d5e186deeaed3cdc37
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-09-27 06:21:50 +0100

    Create the list of landmarks

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ke Ma on 27/09/2020.
+//  Copyright © 2020 Ke. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
