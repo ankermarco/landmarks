34620a10c7654d7a2aab02db368e4eda4640b176
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-09-28 14:30:24 +0100

    Create Badge view

---
+++ Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Ke Ma on 28/09/2020.
+//  Copyright © 2020 Ke. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
