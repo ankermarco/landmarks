99df483e7abc6581cfa37f7d5c8654aebdcb6fce
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-09-29 20:40:21 +0100

    Add navigation between sections

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -12,34 +12,28 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavouritesOnly) {
-                    Text("Favourites only")
-                }
-                
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavouritesOnly || landmark.isFavourite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+        
+        List {
+            Toggle(isOn: $userData.showFavouritesOnly) {
+                Text("Favourites only")
+            }
+            
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavouritesOnly || landmark.isFavourite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle(Text("Landmarks"))
         }
-        
+        .navigationBarTitle(Text("Landmarks"))
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone xs Max", "iPad Pro (12.9-inch)"], id: \.self) {
-            deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }.environmentObject(UserData())
-        
+        NavigationView {
+            LandmarkList().environmentObject(UserData())
+        }
     }
 }
